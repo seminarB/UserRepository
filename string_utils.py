20c54f4bbbbcc7b316388ba9e08467831afbd4e0
TaketoUsui <take20030524@yahoo.co.jp>
2025-12-26 01:14:45 +0900

    [feat]ちょっと変える

--- string_utils.py
+++ string_utils.py
@@ -1,35 +1,36 @@
-def reverse_string(text):
-    return text[::-1]
-
-
-def count_words(text):
-    if not text:
-        return 0
-    return len(text.split())
-
-
-def capitalize_words(text):
-    words = text.split()
-    capitalized = [word.capitalize() for word in words]
-    return ' '.join(capitalized)
-
-
-def remove_whitespace(text):
-    return ''.join(text.split())
-
-
-def is_palindrome(text):
-    cleaned = text.lower().replace(' ', '')
-    return cleaned == cleaned[::-1]
-
-
-def find_substring(text, substring):
-    return text.find(substring)
-
-
-def replace_text(text, old, new):
-    return text.replace(old, new)
-
-
-def to_uppercase(text):
-    return text.upper()
+def reverse_string(text): 
+    return text[::-1] 
+ 
+ 
+def count_words(text): 
+    if not text: 
+        return 0 
+    return len(text.split()) 
+ 
+ 
+def capitalize_words(text): 
+    words = text.split() 
+    capitalized = [word.capitalize() for word in words] 
+    return ' '.join(capitalized) 
+ 
+ 
+def remove_whitespace(text): 
+    return ''.join(text.split()) 
+ 
+ 
+def is_palindrome(text): 
+    cleaned = text.lower().replace(' ', '') 
+    return cleaned == cleaned[::-1] 
+ 
+ 
+def find_substring(text, substring): 
+    return text.find(substring) 
+ 
+ 
+def replace_text(text, old, new): 
+    return text.replace(old, new) 
+ 
+ 
+def to_uppercase(text): 
+    return text.upper() 
+ 
