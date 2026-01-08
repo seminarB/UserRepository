3a0477dd58b249fc825881f5ecbbc15d11e9475e
TaketoUsui <take20030524@yahoo.co.jp>
2026-01-09 00:17:28 +0900

    [feat]差分を発生させる

--- string_utils.py
+++ string_utils.py
@@ -1,36 +1,36 @@
-def reverse_string(text): 
+def reverse_string(text):  
     return text[::-1] 
  
  
-def count_words(text): 
+def count_words(text):  
     if not text: 
         return 0 
     return len(text.split()) 
  
  
-def capitalize_words(text): 
+def capitalize_words(text):  
     words = text.split() 
     capitalized = [word.capitalize() for word in words] 
     return ' '.join(capitalized) 
  
  
-def remove_whitespace(text): 
+def remove_whitespace(text):  
     return ''.join(text.split()) 
  
  
-def is_palindrome(text): 
+def is_palindrome(text):  
     cleaned = text.lower().replace(' ', '') 
     return cleaned == cleaned[::-1] 
  
  
-def find_substring(text, substring): 
+def find_substring(text, substring):  
     return text.find(substring) 
  
  
-def replace_text(text, old, new): 
+def replace_text(text, old, new):  
     return text.replace(old, new) 
  
  
-def to_uppercase(text): 
+def to_uppercase(text):  
     return text.upper() 
  
